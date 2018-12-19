780109a58507b158133ff5f0dce35a5ef9ce8572
Mario Rodriguez <mario@teclaworks.com>
2018-12-19 08:38:00 -0800

    Run docs js when DOM is ready.

--- docs/source/js/mailtoui-docs.js
+++ docs/source/js/mailtoui-docs.js
@@ -1,4 +1,11 @@
 /**
+ * Run when DOM is ready.
+ */
+domReady(function() { 
+    embedVersion();
+});
+
+/**
  * Set dark theme CSS class to show the custom styling example.
  */
 function setDarkTheme() {
@@ -10,5 +17,23 @@ function setDarkTheme() {
 /**
  * Embed version number on docs page.
  */
-var version = require('../../../version.js');
-window.document.getElementById('version').innerHTML = version;
+function embedVersion() {
+    var version = require('../../../version.js');
+
+    window.document.getElementById('version').innerHTML = version;
+}
+
+/**
+ * Proceed only if the DOM is loaded and ready.
+ * 
+ * @param  {Function}   fn  The function to be executed when DOM is ready.
+ */
+function domReady(fn) {
+    // Wait for DOM to be ready, then execute the given function.
+    document.addEventListener("DOMContentLoaded", fn);
+  
+    // If the DOM is ready now, then execute the given function now.
+    if (document.readyState === "interactive" || document.readyState === "complete" ) {
+        fn();
+    }
+}
