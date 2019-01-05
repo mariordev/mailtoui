66a721353124ea031ab383c3b6d139600733178e
Mario Rodriguez <mario@teclaworks.com>
2019-01-05 01:32:05 -0800

    v0.1.10 to fix npm release.

--- dist/mailtoui-min.js
+++ dist/mailtoui-min.js
@@ -1,6 +1,6 @@
 /**
  * mailtoui - A convenient drop-in user interface for mailto links.
- * @version v0.1.9
+ * @version v0.1.10
  * @link https://mailtoui.com
  * @author Mario Rodriguez - https://twitter.com/mariordev
  * @license MIT
