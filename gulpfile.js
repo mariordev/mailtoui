926d0a0a713ab6557a52c025452bd955c64c80e2
Mario Rodriguez <mario@teclaworks.com>
2019-01-10 13:37:38 -0800

    Rework build tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var { series, src, dest, watch } = require('gulp');
+var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify');
 var cleanCSS = require('gulp-clean-css');
@@ -7,7 +7,9 @@ var header = require('gulp-header');
 var browserify = require('gulp-browserify');
 var eslint = require('gulp-eslint');
 var exec = require('gulp-exec');
-var packageJSONFile = require('./package.json');
+const fs = require('fs-extra');
+const htmlmin = require('gulp-htmlmin');
+var packageJSON = null;
 var banner = [
     '/**',
     ' * <%= pkg.name %> - <%= pkg.description %>',
@@ -19,12 +21,21 @@ var banner = [
     '',
 ].join('\n');
 
-function getVersionFromPackage() {
-    return src('./package.json').pipe(exec('genversion version.js'));
+/**
+ * Process HTML files.
+ */
+function libMarkup() {
+    return gulp.src('./src/html/component.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(rename('./src/html/component-min.html'))
+        .pipe(gulp.dest('./'));
 }
 
-function packageCSS() {
-    return src('./src/css/mailtoui.css')
+/**
+ * Process library CSS files.
+ */
+function libStyles() {
+    return gulp.src('./src/css/component.css')
         .pipe(
             autoprefixer({
                 browsers: ['last 2 versions'],
@@ -32,45 +43,96 @@ function packageCSS() {
             })
         )
         .pipe(cleanCSS())
-        .pipe(rename('src/css/mailtoui-min.css'))
-        .pipe(dest('./'));
+        .pipe(rename('./src/css/component-min.css'))
+        .pipe(gulp.dest('./'));
 }
 
-function packageJS() {
-    return src('./src/js/mailtoui.js')
-        .pipe(minify({ noSource: true }))
-        .pipe(header(banner, { pkg: packageJSONFile }))
-        .pipe(dest('dist'));
-}
-
-function docsCSS() {
-    return src(['./docs/source/css/mailtoui-docs.css'])
-        .pipe(cleanCSS())
-        .pipe(rename('docs/assets/css/mailtoui-docs-min.css'))
-        .pipe(dest('./'));
-}
-
-function docsJS() {
-    return src('./docs/source/js/mailtoui-docs.js')
-        .pipe(minify({ noSource: true }))
-        .pipe(browserify())
-        .pipe(dest('docs/assets/js'));
-}
-
-function lintJS() {
-    return src(['src/js/*.js', 'docs/source/js/*.js'])
+/**
+ * Lint library JavaScript files.
+ */
+function libScriptsLint() {
+    return gulp.src('./src/js/mailtoui.js')
         .pipe(eslint())
         .pipe(eslint.format());
 }
 
-function watching() {
-    watch('src/css/*.css', packageCSS);
-    watch(['src/js/*.js', './package.json'], series(getVersionFromPackage, packageJS));
-
-    watch('docs/source/css/*.css', docsCSS);
-    watch(['docs/source/js/*.js', './package.json'], series(getVersionFromPackage, docsJS));
-
-    watch(['src/js/*.js', 'docs/source/js/*.js'], lintJS);
+/**
+ * Process library JavaScript files.
+ */
+function libScripts() {
+    return gulp.src('./src/js/mailtoui.js')
+        .pipe(minify({ noSource: true }))
+        .pipe(header(banner, { pkg: packageJSON }))
+        .pipe(gulp.dest('dist'));
 }
 
-exports.default = series(getVersionFromPackage, packageCSS, packageJS, docsCSS, docsJS, lintJS, watching);
+/**
+ * Process docs CSS files.
+ */
+function docsStyles() {
+    return gulp.src('./docs/source/css/mailtoui-docs.css')
+        .pipe(cleanCSS())
+        .pipe(rename('./docs/assets/css/mailtoui-docs-min.css'))
+        .pipe(gulp.dest('./'));
+}
+
+/**
+ * Lint docs JavaScript files.
+ */
+function docsScriptsLint() {
+    return gulp.src('./docs/source/js/mailtoui-docs.js')
+        .pipe(eslint())
+        .pipe(eslint.format());
+}
+
+/**
+ * Process docs JavaScript files.
+ */
+function docsScripts() {
+    return gulp.src('./docs/source/js/mailtoui-docs.js')
+        .pipe(minify({ noSource: true }))
+        .pipe(browserify())
+        .pipe(gulp.dest('./docs/assets/js'));
+}
+
+/**
+ * Read package.json and update version.js. Both are needed
+ * to update the library header and version shown in docs.
+ */
+function getPackageJSON() {
+    packageJSON = fs.readJsonSync('./package.json');
+
+    return gulp.src('./package.json').pipe(exec('genversion version.js'));
+}
+
+/**
+ * The all seeing eye...
+ */
+function watchFiles() {
+    gulp.watch('./package.json', getPackageJSON);
+    gulp.watch('./src/css/mailtoui.css', libStyles);
+    gulp.watch(['./src/js/mailtoui.js', './version.js'], gulp.series(libScriptsLint, libScripts));
+    gulp.watch('./docs/source/css/mailtoui-docs.css', docsStyles);
+    gulp.watch(['./docs/source/js/mailtoui-docs.js', './version.js'], gulp.series(docsScriptsLint, docsScripts))
+}
+
+/**
+ * Define complex tasks.
+ */
+const libJS = gulp.series(libScriptsLint, libScripts);
+const docsJS = gulp.series(docsScriptsLint, docsScripts);
+const build = gulp.series(getPackageJSON, libMarkup, gulp.parallel(libStyles, libScripts), gulp.parallel(docsStyles, docsScripts));
+const watch = gulp.series(build, watchFiles);
+
+/**
+ * Make tasks available to the outside world.
+ */
+exports.libHTML = libMarkup;
+exports.libCSS = libStyles;
+exports.libJSLint = libScriptsLint;
+exports.libJS = libJS;
+exports.docsCSS = docsStyles;
+exports.docsJSLint = docsScriptsLint;
+exports.docsJS = docsJS;
+exports.watch = watch;
+exports.default = build;
